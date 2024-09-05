0889509d5c01fee7155dc2aa6b09c85ce132000f
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-09-05 20:37:14 +0300

    t07 t08

---
+++ t07/mx_sort_arr_int.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void mx_sort_arr_int(int*arr, int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 
+            && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
