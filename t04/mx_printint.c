02e23ec8cd5dcacbff60762564376d1e3f870689
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-09-05 20:36:51 +0300

    t04

---
+++ t04/mx_printint.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void mx_printchar(char c);
+
+void mx_printint(int n) {    
+    if (n == -2147483648){
+        write(1, "-2147483648", 11);
+    }
+    else { 
+        if (n < 0) {
+            n = -n;
+            mx_printchar('-');
+        }
+        if (n/10 >= 1) {
+            mx_printint(n/10);
+        }
+        mx_printchar(n%10 + 48);
+    }
+}
